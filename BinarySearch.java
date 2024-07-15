9b2646a83a45a2fc6715e202398fed41f4ee27ef
narendramongam <mongamnarendra@gmail.com>
2024-07-15 22:37:43 +0530

    BinarySearch

---
+++ BinarySearch.java
@@ -0,0 +1,29 @@
+package NPTEL_CP;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7};
+        int key = 1;
+        int index = binarySearch(nums,key,0,nums.length);
+        System.out.println(index+1);
+    }
+
+    private static int binarySearch(int[] nums, int key,int start,int end) {
+        if(start <= end)
+        {
+            int mid = (start+end)/2;
+            if(nums[mid]==key)
+            {
+                return mid;
+            }
+            else{
+
+                return nums[mid] < key ? binarySearch(nums, key, mid + 1, end) : binarySearch(nums, key, start, mid-1);
+            }
+        }
+        else {
+            return -1;
+        }
+
+    }
+}
