93d7d3f1bfc0fd9f104a603b7219ee1d8ad0b7bf
narendramongam <mongamnarendra@gmail.com>
2024-07-15 22:41:16 +0530

    SortingTech

--- Reversort.java
+++ Reversort.java
@@ -6,6 +6,7 @@ public class Reversort {
     public static void main(String[] args) {
         int[] nums = {5,4,3,2,1};
         ReverSort(nums);
+
     }
 
     private static void ReverSort(int[] nums) {
