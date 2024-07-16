2591831c2cdba98c5aa9b9fe642015f2b88d0c63
narendramongam <mongamnarendra@gmail.com>
2024-07-16 23:38:56 +0530

    leetcode-74

--- SearchIn2D.java
+++ SearchIn2D.java
@@ -1,31 +1,33 @@
 package NPTEL_CP;
 
-public class Sample {
+public class SearchIn2D {
     public static void main(String[] args) {
         int[][] nums={{1,3,5,7},{10,11,16,20},{23,30,34,60}};
-        int key = 600;
-        int index =-1;
-        for(int i=0;i< nums.length;i++)
-        {
-            int len = nums[i].length-1;
+        int key = 5;
+        int ans = Dim(nums,key);
 
-            if(nums[i][len] >= key)
-            {
-                index= searchBinary(nums[i],key,0,len+1);
-                if(index==-1)
-                {
-                    System.out.println("not found");
-                    break;
-                }
-                else {
-                    System.out.println("found at "+index);
-                    break;
+
+
+    }
+
+    private static int Dim(int[][] nums,int key)
+    {
+        int index =-1;
+        for(int i=0;i< nums.length;i++) {
+            int len = nums[i].length - 1;
+
+            if (nums[i][len] >= key) {
+                index = searchBinary(nums[i], key, 0, len + 1);
+                if (index == -1) {
+                    return -1;
+                } else {
+                    return index;
                 }
             }
         }
-        System.out.println("not found");
-
+        return -1;
     }
+
 
     private static int searchBinary(int[] nums, int key,int start,int end) {
         if(start<=end)
